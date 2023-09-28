5f1ca07e81c742de751e08a3a90176f7bc52e45e
Iulia <robert.lantus@gmail.com>
2023-09-28 20:51:27 +0300

    slide from right menu

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ btn.addEventListener('click', () => {
     header.classList.toggle('open');
     ul.classList.toggle('open');
 });
+
+// Full screen Overlay Navigation
+
+// 1 -- Slide in from the right side
+
+document.querySelector('.openbtn').addEventListener('click', () => {
+    document.getElementById("myNav").style.width = "20rem";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+});
+
+document.querySelector('.closebtn').addEventListener('click', () => {
+    document.getElementById("myNav").style.width = "0%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+});
